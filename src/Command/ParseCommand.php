53c264cd67e6929b2fb42fd5ccca9a8459a204b0
Elias Rudberg <eli.rud@hotmail.com>
2020-01-29 22:18:31 +0100

    Change functionality to work completely within the CLI
    Change the functionality of the project to work completely within the
    CLI instead of opening a webpage to display results. Guessing that's
    more inline with the assignment, and seems easier to work with.

---
+++ src/Command/ParseCommand.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ParseCommand extends Command
+{
+    protected static $defaultName = 'app:parse-files';
+
+    protected function configure()
+    {
+        $this->setDescription('Parse file(s) for dependencies.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        return 0;
+    }
+}
