a44bdd38db0ec4ec8505a97279f47e99d8b76aed
Elias Rudberg <eli.rud@hotmail.com>
2020-02-03 00:12:58 +0100

    Add printing of json dependencies in table form

--- src/Command/ParseCommand.php
+++ src/Command/ParseCommand.php
@@ -2,6 +2,8 @@
 namespace App\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableSeparator;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Finder\Finder;
@@ -18,32 +20,37 @@ class ParseCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $finder = new Finder();
-
+        $table = new Table($output);
+        $table->setHeaders(['Product', 'Version']);
+        $rows = array();
         $finder->files()->in(".\public\\files");
 
         if ($finder->hasResults()) {
             foreach ($finder as $file) {
                 $contents = $file->getContents();
                 $extension = $file->getExtension();
-                $output->writeln($file->getRelativePathName());
+                $fileName = $file->getRelativePathName();
                 if ($extension == "json") {
-                    $dependencies = $this->get_json_dependencies($contents);
+                    $rows = $this->get_json_dependencies($contents, $rows);
+                    $table->setHeaderTitle('Dependencies found in '.$fileName);
                 } else {
                     $dependencies = $this->get_lock_dependencies($contents);
                 }
                 $output->writeln($dependencies);
+                $table->setRows($rows);
+                $table->render();
             }
         }
         return 0;
     }
 
-    public function get_json_dependencies($fileContent)
+    public function get_json_dependencies($fileContent, $rows)
     {
         $contentAsJson = json_decode($fileContent, true);
-        foreach ($contentAsJson["dependencies"] as $x => $x_value) {
-            echo "Key=" . $x . ", Value=" . $x_value;
+        foreach ($contentAsJson["dependencies"] as $product => $version) {
+            array_push($rows, new TableSeparator(), [$product, $version]);
         }
-        return "";
+        return $rows;
     }
 
     public function get_lock_dependencies($fileContent)
