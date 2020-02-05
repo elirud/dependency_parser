966b832399bd1ea4a1d3575d0b22a9ae19b3bd9b
Elias Rudberg <eli.rud@hotmail.com>
2020-02-05 18:29:55 +0100

    Add test testing empty directory as input

---
+++ tests/ParseCommandTest.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class CreateUserCommandTest extends KernelTestCase
+{
+    public function testExecute()
+    {
+        $kernel = static::createKernel();
+        $application = new Application($kernel);
+
+        // command to execute
+        $command = $application->find('app:parse-files');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            // option 'dir' sent to command input
+            '--dir' => '.\public\test\emptyDir'
+        ]);
+
+        // the output of the command in the console
+        $output = $commandTester->getDisplay();
+
+        $this->assertContains('No files found in the ' .
+                                '.\public\test\emptyDir directory.', $output);
+    }
+}
