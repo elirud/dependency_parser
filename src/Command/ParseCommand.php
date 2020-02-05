4d2c0be6f240f2403563eb585748371aaf572f16
Elias Rudberg <eli.rud@hotmail.com>
2020-02-05 19:59:28 +0100

    Add comments and documentaion

--- src/Command/ParseCommand.php
+++ src/Command/ParseCommand.php
@@ -13,17 +13,18 @@ use function Symfony\Component\String\u;
 
 class ParseCommand extends Command
 {
+    // the command you call from the CLI after 'php bin/console'
     protected static $defaultName = 'app:parse-files';
 
     protected function configure()
     {
         $this->setDescription('Parse file(s) for dependencies.');
         $this->addOption(
-            'dir',
+            'dir',   // option name, used to call with --dir
             null,
             InputOption::VALUE_REQUIRED,
             'Which directory to search for files to parse for dependencies',
-            ".\public\\files"
+            ".\public\\files" // default value
         );
     }
 
@@ -39,9 +40,11 @@ class ParseCommand extends Command
         if ($finder->hasResults()) {
             $output->writeln("Dependencies found for:");
             foreach ($finder as $file) {
+                // Get file information and contents
                 $contents = $file->getContents();
                 $extension = $file->getExtension();
                 $fileName = $file->getRelativePathName();
+                // Check file extension and call appropriate function.
                 if ($extension == "json") {
                     $rows = $this->get_json_dependencies($contents, $rows);
                     $table->setHeaderTitle($fileName);
@@ -49,6 +52,7 @@ class ParseCommand extends Command
                     $rows = $this->get_lock_dependencies($contents, $rows);
                     $table->setHeaderTitle($fileName);
                 }
+                // Render table if there are found dependencies.
                 if (!empty($rows)) {
                     $table->setRows($rows);
                     $table->render();
@@ -65,7 +69,18 @@ class ParseCommand extends Command
         return 0;
     }
 
-    public function get_json_dependencies($fileContent, $rows)
+    /**
+    * We get the dependencies from the json file by converting
+    * the json string to an associative array, and get them by
+    * checking the value for the key 'dependencies'. Here we assume
+    * the json string contains a key 'dependencies' which in turn
+    * points to an array with product name as key and version as value.
+    *
+    * @param $fileContent The content of the file to parse.
+    * @param $rows The array where we put the rows for the eventual table.
+    * @return $rows Array with the final rows for the eventual table.
+    */
+    private function get_json_dependencies($fileContent, $rows)
     {
         $contentAsJson = json_decode($fileContent, true);
         if (!isset($contentAsJson["dependencies"])) {
@@ -77,7 +92,18 @@ class ParseCommand extends Command
         return $rows;
     }
 
-    public function get_lock_dependencies($fileContent, $rows)
+    /**
+    * We get the dependencies from the lock file through some simple
+    * string operations using Symfonys UnicodeString. Here we assume
+    * that 'DEPENDENCIES' and the next 'key' after are seperated by
+    * a blankline (two newlines), and that the first whitespace for
+    * a dependency is between the products name and its version.
+    *
+    * @param $fileContent The content of the file to parse.
+    * @param $rows The array where we put the rows for the eventual table.
+    * @return $rows Array with the final rows for the eventual table.
+    */
+    private function get_lock_dependencies($fileContent, $rows)
     {
         if (strpos($fileContent, 'DEPENDENCIES') == false) {
             return $rows;
